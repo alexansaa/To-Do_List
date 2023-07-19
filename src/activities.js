cfab412f312b194651635ede70c717498bbc01cc
alexansaa <alexan.saa@gmail.com>
2023-07-19 11:24:20 -0500

    review fix

--- src/activities.js
+++ src/activities.js
@@ -26,24 +26,6 @@ export default class Activities {
     },
   ];
 
-  static addNewActivitie(description, completed) {
-    const NewActivitie = new Activities(description, completed);
-    Activities.activities.push(NewActivitie);
-    Activities.updateData();
-  }
-
-  static updateActivitie(description, completed, index) {
-    const targetObj = Activities.activities.find((obj) => obj.index === parseInt(index, 10));
-    targetObj.description = description;
-    targetObj.completed = completed;
-    Activities.updateData();
-  }
-
-  static removeActivite(index) {
-    Activities.activities = Activities.activities.filter((item) => item.index !== index);
-    Activities.updateData();
-  }
-
   static updateData() {
     localStorage.setItem('activities', JSON.stringify(Activities.activities));
   }
