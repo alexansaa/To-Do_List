e6136ce98817839f461a3c9fb1a2ca4667994ea9
alexansaa <alexan.saa@gmail.com>
2023-07-27 14:58:40 -0500

    Linter fix

--- add.test.js
+++ add.test.js
@@ -1,51 +1,50 @@
 import {describe, expect, test} from '@jest/globals';
-import Activities from "./src/activities";
-import { localstorageMock } from './localstorageMock';
-const ActivitiesObj = require("./src/activities");
+import Activities from './src/activities';
+import { localstorageMock } from './localstorageMock.js';
 
-describe("Activities add functionality", () => {
+describe('Activities add functionality', () => {
   // test constructor overload (description, complete)
-  test("defines addNewActivitie(overload1)", () => {
+  test('defines addNewActivitie(overload1)', () => {
     // Arrange
-    const myDescription = "activity description";
+    const myDescription = 'activity description';
     const myComplete = true;
     // Act
-    const myAct = new Activities(myDescription,myComplete);
+    const myAct = new Activities(myDescription, myComplete);
     // Assert
-    expect(myAct.description).toBe("activity description");
+    expect(myAct.description).toBe('activity description');
     expect(myAct.completed).toBeTruthy();
   });
 
   // test constructor overload (description, complete, index)
-  test("defines addNewActivitie(overload2)", () => {
+  test('defines addNewActivitie(overload2)', () => {
     // Arrange
-    const myDescription = "activity description";
+    const myDescription = 'activity description';
     const myComplete = true;
     const myIndex = 1;
     // Act
-    const myAct2 = new Activities(myDescription,myComplete,myIndex);
+    const myAct2 = new Activities(myDescription, myComplete, myIndex);
     // Assert
-    expect(myAct2.description).toBe("activity description");
+    expect(myAct2.description).toBe('activity description');
     expect(myAct2.completed).toBeTruthy();
     expect(myAct2.index).toBe(1);
   });
 
   // test add item method
-  test("defines add item method", () => {
+  test('defines add item method', () => {
     // Assert
-    expect(typeof Activities.addNewActivitie).toBe("function");
+    expect(typeof Activities.addNewActivitie).toBe('function');
   });
 
-  test("correctly adding new item", () => {
+  test('correctly adding new item', () => {
     // Arrange
-    const act1Desc = "Activity 1";
+    const act1Desc = 'Activity 1';
     const act1Comp = true;
     // Act
     Activities.addNewActivitie(act1Desc, act1Comp);
     // Assert
     expect(Activities.activities.length).toBe(1);
-    expect(typeof Activities.activities[0]).toBe("object");
-    expect(Activities.activities[0].description).toBe("Activity 1");
+    expect(typeof Activities.activities[0]).toBe('object');
+    expect(Activities.activities[0].description).toBe('Activity 1');
     expect(Activities.activities[0].completed).toBeTruthy();
   });
 });
--- localstorageMock.js
+++ localstorageMock.js
@@ -2,10 +2,16 @@
 const localStorageMock = (() => {
   let store = {};
   return {
-    getItem: key => store[key] || null,
-    setItem: (key, value) => (store[key] = value.toString()),
-    removeItem: key => delete store[key],
-    clear: () => (store = {}),
+    getItem: key => {
+      store[key] || null
+    },
+    setItem: (key, value) => {
+      store[key] = value.toString()
+    },
+    removeItem: key => { delete store[key] },
+    clear: () => {
+      store = {}
+    },
   };
 })();
 
